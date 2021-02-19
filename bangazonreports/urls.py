ba50584f78428964d2427fc76242363a86381650
Jonathan Shearon <jonathan.shearon@gmail.com>
2021-02-18 22:13:19 -0600

    completed orders report

--- bangazonreports/urls.py
+++ bangazonreports/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import products_over_1000
+from .views import products_over_1000, completed_orders
 
 urlpatterns = [
     path('reports/expensiveproducts', products_over_1000),
+    path('reports/completedorders', completed_orders),
 ]
--- bangazonreports/views/__init__.py
+++ bangazonreports/views/__init__.py
@@ -1,2 +1,3 @@
 from .connection import Connection
 from .products.expensive_products import products_over_1000
+from .orders.completed_orders import completed_orders
